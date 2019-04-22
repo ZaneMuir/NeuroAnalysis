43924e38b1e2c40226121809d264914cc1bc8a4f
zane muir <zanemuir1995@gmail.com>
2019-04-22 11:24:59 +0800

    detect cross point

--- neuroanalysis/waveform/misc.py
+++ neuroanalysis/waveform/misc.py
@@ -15,3 +15,44 @@ def create_1d_epoch_bymarker(data, marker, roi, fs, mbias=0):
         start = int(np.floor((eachm + roi[0] + mbias) * fs))
         result[midx, :] = data[start:start+gap]
     return result
+
+def detect_cross_pnt(arr, thr, way='up', gap=1):
+    """
+    detect the data rise/down point, returns the index of the
+    point right above the threshold.
+
+    arguments:
+    - arr: data array (1d)
+    - thr: threshold (scale)
+
+    key arguments:
+    - way: either be "up" or "down", for data rise/ data down respectively.
+    - gap: the least points between two valid markers.
+
+    returns:
+    - _marker_idx: index array (1d)
+    """
+
+    _idx, = np.where(arr > thr)
+    _idx_diff, = np.where(np.diff(_idx) > 1)
+    _idx_repo = np.hstack((_idx[0], _idx[_idx_diff], _idx[_idx_diff+1], _idx[-1]))
+
+    if way == 'up':
+        _check = lambda x, i: x[i-1] < thr < x[i] #< x[i+1]
+    elif way =='down':
+        _check = lambda x, i: x[i-1] > x[i] > x[i+1]
+    else:
+        raise ValueError("unknown `way` value.")
+
+    _idx_repo.sort()
+    _result = []
+    _previous = -9999
+    for idx in _idx_repo:
+        try:
+            if _check(arr, idx) and (idx - _previous > gap):
+                _result.append(idx)
+                _previous = idx
+        except IndexError:
+            pass
+
+    return _result
